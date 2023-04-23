9aacbed3dc870aa7121d26af30b15beb3b97df6a
Nordin Belkacemi <belkaceminordin@gmail.com>
2023-04-23 16:14:34 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from hyps import batch_size, dataset_repetitions, image_size
+
+dataset_name = "oxford_flowers102"
+
+
+def preprocess_image(data):
+    # center crop image
+    height = tf.shape(data["image"])[0]
+    width = tf.shape(data["image"])[1]
+    crop_size = tf.minimum(height, width)
+    image = tf.image.crop_to_bounding_box(
+        data["image"],
+        (height - crop_size) // 2,
+        (width - crop_size) // 2,
+        crop_size,
+        crop_size,
+    )
+
+    # resize and clip
+    # for image downsampling it is important to turn on antialiasing
+    image = tf.image.resize(
+        image, size=[image_size, image_size], antialias=True)
+    return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+
+def prepare_dataset(split):
+    # the validation dataset is shuffled as well, because data order matters
+    # for the KID estimation
+    return (
+        tfds.load(dataset_name, split=split, shuffle_files=True)
+        .map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+        .cache()
+        .repeat(dataset_repetitions)
+        .shuffle(10 * batch_size)
+        .batch(batch_size, drop_remainder=True)
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+    )
+
+
+# load dataset
+train_dataset = prepare_dataset("train[:80%]+validation[:80%]+test[:80%]")
+val_dataset = prepare_dataset("train[80%:]+validation[80%:]+test[80%:]")
